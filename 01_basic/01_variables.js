e3fc270114a806835994244f4d76366cb1d1251a
Anoop-kumar97 <159508164+Anoop-kumar97@users.noreply.github.com>
2024-07-25 12:06:03 +0000

    Data types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "978575932309";
+let accountEmail = "Anoop@gmail.com";
+var accountPass = "9876543";
+accountCity = "jaipur";
+
+ //accountId = "4839348";
+accountEmail = "jaat@gmail.com";
+accountPass = "98475893";
+accountCity = "bengluru";
+
+console.log(accountId);
+
+ console.table([accountId,accountEmail,accountPass,accountCity]);
+
+
+ 
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using node js, not browser.
+
+console.log(3
+    +4)  //  code readability should be high.
+console.log("anoop")
+
+let name = "anoop"
+let age = 25
+let city;
+let avilable = true
+
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// srting => ""
+// null => standlone value
+// undefined =>
+// symbul => unique
