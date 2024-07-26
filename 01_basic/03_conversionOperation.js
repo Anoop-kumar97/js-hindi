c4ffe493f9e84271e6201e9b8fbea40e217cb6c2
Anoop-kumar97 <159508164+Anoop-kumar97@users.noreply.github.com>
2024-07-26 11:38:28 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,66 @@
+// let mark = 33
+// let marks = "37"
+// let total =  "70 All Mark"
+
+// console.log(typeof mark)    // number
+// console.log(typeof marks)   // string
+// console.log(typeof total)   // string
+
+// let value = Number(marks)
+// console.log(typeof value)   // number
+
+// let allMark = Number(total)
+// console.log(typeof allMark) 
+// console.log(allMark)  //  show number. But this is NaN ( NOT A NUMBER) !.
+
+
+// let textN = null
+// let textN1 = Number(textN)
+// console.log(typeof textN1)   // number
+// console.log(textN1)    //  value define = 0
+ 
+
+// let textBool = false
+// let textBool1 = Number(textBool)
+// console.log(typeof textBool1)  // number
+// console.log(textBool1)         // value define true = 1  / false = 0 .
+
+// let textUnf = undefined
+// let textUnf1 = Number(textUnf)
+// console.log(typeof textUnf1)    // number
+// console.log(textUnf1)           // undefined  -  value = NaN
+
+
+// let check = undefined
+// let boolCheck = Boolean(check)
+// console.log(boolCheck)
+
+// //  0 => false
+// //  1 => true
+// //  "" => false
+// //  "abcd" => true
+
+// let info = false
+// let infoStr = String(info)
+// console.log(typeof infoStr)   //  string
+// console.log(infoStr)          //  value = 35   /  same value add to first variables.
+
+
+
+// ******************************  OPARETIONS  **********************************// 
+
+
+let say = "Hello";
+let name = " Anoop";
+console.log(say + name);
+
+ /*   {        This  code  is not review   ti's code type is not Readable
+        console.log( 3+5+6 +"10");   //  output =>  1410    
+        console.log("5"+ 2+3+4);     //  output =>  5234
+        console.log("5", 2+3+4+5);   //  output =>  5  14   
+        console.log( (2+3)*10%5);      
+        console.log( +true )         //  output => 1  / (true) => true
+        console.log( +"")            // output  => 0
+        let num1 , num2 , num3
+        num1 = num2 = num2 = 2+2     //  output  => 4          }    */
+
