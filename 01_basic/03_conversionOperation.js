79a1fc53d399304864622229f1d39191f4b8afcb
Anoop-kumar97 <159508164+Anoop-kumar97@users.noreply.github.com>
2024-07-26 11:52:07 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,49 +1,49 @@
-// let mark = 33
-// let marks = "37"
-// let total =  "70 All Mark"
+let mark = 33
+let marks = "37"
+let total =  "70 All Mark"
 
-// console.log(typeof mark)    // number
-// console.log(typeof marks)   // string
-// console.log(typeof total)   // string
+console.log(typeof mark)    // number
+console.log(typeof marks)   // string
+console.log(typeof total)   // string
 
-// let value = Number(marks)
-// console.log(typeof value)   // number
+let value = Number(marks)
+console.log(typeof value)   // number
 
-// let allMark = Number(total)
-// console.log(typeof allMark) 
-// console.log(allMark)  //  show number. But this is NaN ( NOT A NUMBER) !.
+let allMark = Number(total)
+console.log(typeof allMark) 
+console.log(allMark)  //  show number. But this is NaN ( NOT A NUMBER) !.
 
 
-// let textN = null
-// let textN1 = Number(textN)
-// console.log(typeof textN1)   // number
-// console.log(textN1)    //  value define = 0
+let textN = null
+let textN1 = Number(textN)
+console.log(typeof textN1)   // number
+console.log(textN1)    //  value define = 0
  
 
-// let textBool = false
-// let textBool1 = Number(textBool)
-// console.log(typeof textBool1)  // number
-// console.log(textBool1)         // value define true = 1  / false = 0 .
+let textBool = false
+let textBool1 = Number(textBool)
+console.log(typeof textBool1)  // number
+console.log(textBool1)         // value define true = 1  / false = 0 .
 
-// let textUnf = undefined
-// let textUnf1 = Number(textUnf)
-// console.log(typeof textUnf1)    // number
-// console.log(textUnf1)           // undefined  -  value = NaN
+let textUnf = undefined
+let textUnf1 = Number(textUnf)
+console.log(typeof textUnf1)    // number
+console.log(textUnf1)           // undefined  -  value = NaN
 
 
-// let check = undefined
-// let boolCheck = Boolean(check)
-// console.log(boolCheck)
+let check = undefined
+let boolCheck = Boolean(check)
+console.log(boolCheck)
 
-// //  0 => false
-// //  1 => true
-// //  "" => false
-// //  "abcd" => true
+//  0 => false
+//  1 => true
+//  "" => false
+//  "abcd" => true
 
-// let info = false
-// let infoStr = String(info)
-// console.log(typeof infoStr)   //  string
-// console.log(infoStr)          //  value = 35   /  same value add to first variables.
+let info = false
+let infoStr = String(info)
+console.log(typeof infoStr)   //  string
+console.log(infoStr)          //  value = 35   /  same value add to first variables.
 
 
 
