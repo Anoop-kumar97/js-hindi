34298605f7649f7714be798d350404542eaaed2d
Anoop-kumar97 <159508164+Anoop-kumar97@users.noreply.github.com>
2024-07-27 03:50:48 +0000

    Comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(null > 0)     //  false
+// console.log(null == 0)    //  false
+// console.log(null >= 0)    //  true
+
+//   {  The reason is that equality check == and comparision < <= > >= work differently.
+//      Comparisions convert null to a number, treating it as 0.
+//      That's why (3) >= 0 is true and (1) null > 0 is false.           }
+
+console.log("5" == 5)   // true
+console.log("5" === 5)  // false
+console.log( 5 === 5)   // true
+console.log(5 == 5)     // true
