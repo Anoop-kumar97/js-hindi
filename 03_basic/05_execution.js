d71355812957db29255743e1626e90b722908faa
Anoop-kumar97 <159508164+Anoop-kumar97@users.noreply.github.com>
2024-08-08 15:44:11 +0000

    Execution

---
+++ 03_basic/05_execution.js
@@ -0,0 +1,10 @@
+//           JAVASCRIPT EXECUTION  CONTEXT
+//    1. Global Execution context
+//    2. Function Execution Context
+//    3. Eval Execution context
+
+
+//   { Code } => Run Two Type Phase
+//    1. Memory Creation Phase
+//    2. Execution Phase
+
